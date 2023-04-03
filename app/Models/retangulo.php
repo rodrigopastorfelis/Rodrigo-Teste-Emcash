5300b1e0d2158d3e43f01dba4bb6f8ca91941562
unknown <rodrigopastorfelis@gmail.com>
2023-04-03 14:37:48 -0300

    UpperCase

--- app/Models/retangulo.php
+++ app/Models/retangulo.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class retangulo extends Model
+class Retangulo extends Model
 {
     use HasFactory;
     protected $fillable = ['base', 'altura', 'area'];
--- app/Models/triangulo.php
+++ app/Models/triangulo.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class triangulo extends Model
+class Triangulo extends Model
 {
     use HasFactory;
 
