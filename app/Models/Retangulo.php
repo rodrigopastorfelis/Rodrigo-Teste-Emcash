ef154c6cbe8d122ec4cd785bc8a844cda19e62db
unknown <rodrigopastorfelis@gmail.com>
2023-04-03 14:53:03 -0300

    upperModel

---
+++ app/Models/Retangulo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Retangulo extends Model
+{
+    use HasFactory;
+    protected $fillable = ['base', 'altura', 'area'];
+
+    public function getArea($base, $altura){
+        return $base * $altura;
+    }
+}
